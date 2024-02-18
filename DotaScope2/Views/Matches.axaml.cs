ebe032ec098002caea490d036908459afac676d8
StariyMicrozavr <artjomilin03@gmail.com>
2024-02-18 22:48:43 +0300

    matches issues

--- DotaScope2/Views/Matches.axaml.cs
+++ DotaScope2/Views/Matches.axaml.cs
@@ -33,18 +33,37 @@ namespace DotaScope2.Views
             await ((MatchesViewModel)DataContext).GetDataFromServer();
         }
 
- //       private void InitializeComponent()
- //       {
- //           AvaloniaXamlLoader.Load(this);
- //           if (isVertical == true)
- //           {
- //                TextBlock textBlock = this.FindControl<TextBlock>("MatchesText");
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+            if (isVertical == true)
+            {
+                TextBlock textBlock = this.FindControl<TextBlock>("MatchesText");
+                textBlock.IsVisible = false;
 
-                // Удаление всех дочерних элементов из Grid
- //               textBlock.IsVisible = false;
-                // Добавление Grid в окно
-//                Content = textBlock;
-//            }
-//        }
+                Grid topBackground = this.FindControl<Grid>("TopBackground");
+                topBackground.RowDefinitions[0].Height = new GridLength(360);
+
+                Grid rank = this.FindControl<Grid>("GridRank");
+                rank.Margin = new Avalonia.Thickness(0, 20, 0, 20);
+                rank.HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center;
+                Grid.SetRow(rank, 1);
+
+                Grid matchesHeaderGrid = this.FindControl<Grid>("MatchesHeaderGrid");
+                Grid.SetRow(matchesHeaderGrid, 2);
+
+
+                Grid avatar = this.FindControl<Grid>("GridAvatar");
+                avatar.Margin = new Avalonia.Thickness(150, 20, 0, 0);
+                Grid.SetRow(avatar, 0);
+
+                Grid playerName = this.FindControl<Grid>("PlayerName");
+                //playerName.Margin = new Avalonia.Thickness(150, 0, 0, 200);
+
+                //Grid matchesGrid = this.FindControl<Grid>("MatchesGrid");
+                //ItemsControl matchesHeaderGrid = this.FindControl<ItemsControl>("MatchesHeaderGrid");
+
+            }
+        }
     }
 }
