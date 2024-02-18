cfdce84ea58320a7a2ac3c908d30798cbf53cf75
StariyMicrozavr <artjomilin03@gmail.com>
2024-02-18 17:56:07 +0300

    Little changes in matches

--- DotaScope2/Views/Matches.axaml.cs
+++ DotaScope2/Views/Matches.axaml.cs
@@ -1,5 +1,8 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.LogicalTree;
+using Avalonia.Markup.Xaml;
 using DotaScope2.ViewModels;
 using System;
 using System.Net.Http;
@@ -9,21 +12,39 @@ namespace DotaScope2.Views
     public partial class Matches: UserControl
     {
         public string playerID;
+        private bool isVertical = false;
+        protected bool IsVertical()
+        {
+            var boundsWidth = this.Bounds.Width;
+            var boundsHeight = this.Bounds.Height;
+            return boundsHeight >= boundsWidth;
+        }
         public Matches()
         {
+            isVertical = IsVertical();
             InitializeComponent();
             DataContext = new MatchesViewModel();
             LoadData();
 
-        }
-        public void toTeams(object source, RoutedEventArgs args)
-        {
-            Console.WriteLine("Переход в Teams");
         }
         private async void LoadData()
         {
             // Загрузка данных из сервера при запуске
             await ((MatchesViewModel)DataContext).GetDataFromServer();
         }
+
+ //       private void InitializeComponent()
+ //       {
+ //           AvaloniaXamlLoader.Load(this);
+ //           if (isVertical == true)
+ //           {
+ //                TextBlock textBlock = this.FindControl<TextBlock>("MatchesText");
+
+                // Удаление всех дочерних элементов из Grid
+ //               textBlock.IsVisible = false;
+                // Добавление Grid в окно
+//                Content = textBlock;
+//            }
+//        }
     }
 }
