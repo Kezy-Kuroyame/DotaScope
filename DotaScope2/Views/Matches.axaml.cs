0729447ee13b908c5f9683845a22f2a55b98a6e6
Kezy <Kezy2807@gmail.com>
2024-02-19 00:00:38 +0300

    Matches android

--- DotaScope2/Views/Matches.axaml.cs
+++ DotaScope2/Views/Matches.axaml.cs
@@ -3,6 +3,7 @@ using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.LogicalTree;
 using Avalonia.Markup.Xaml;
+using Avalonia.VisualTree;
 using DotaScope2.ViewModels;
 using System;
 using System.Net.Http;
@@ -12,17 +13,18 @@ namespace DotaScope2.Views
     public partial class Matches: UserControl
     {
         public string playerID;
-        private bool isVertical = false;
         protected bool IsVertical()
         {
-            var boundsWidth = this.Bounds.Width;
-            var boundsHeight = this.Bounds.Height;
+            Grid mainWindow = this.FindControl<Grid>("mainWindow");
+            var boundsWidth = mainWindow.Bounds.Width;
+            var boundsHeight = mainWindow.Bounds.Height;
+            System.Diagnostics.Debug.WriteLine($"Ширина окна Matches: {boundsWidth}, высота окна Matches: {boundsHeight}");
             return boundsHeight >= boundsWidth;
         }
         public Matches()
         {
-            isVertical = IsVertical();
             InitializeComponent();
+            Loaded += ResizeComponents;
             DataContext = new MatchesViewModel();
             LoadData();
 
@@ -33,11 +35,17 @@ namespace DotaScope2.Views
             await ((MatchesViewModel)DataContext).GetDataFromServer();
         }
 
-        private void InitializeComponent()
+        private void ResizeComponents(object sender, EventArgs e)
         {
-            AvaloniaXamlLoader.Load(this);
-            if (isVertical == true)
+            if (IsVertical())
             {
+                ItemsControl AndroidName = this.FindControl<ItemsControl>("AndroidName");
+                AndroidName.IsVisible = true;
+
+                Grid MatchesHeaderGridAndroid = this.FindControl<Grid>("MatchesHeaderGridAndroid");
+                MatchesHeaderGridAndroid.IsVisible = true;
+          
+
                 TextBlock textBlock = this.FindControl<TextBlock>("MatchesText");
                 textBlock.IsVisible = false;
 
@@ -50,6 +58,7 @@ namespace DotaScope2.Views
                 Grid.SetRow(rank, 1);
 
                 Grid matchesHeaderGrid = this.FindControl<Grid>("MatchesHeaderGrid");
+                matchesHeaderGrid.IsVisible = false;
                 Grid.SetRow(matchesHeaderGrid, 2);
 
 
@@ -57,13 +66,18 @@ namespace DotaScope2.Views
                 avatar.Margin = new Avalonia.Thickness(150, 20, 0, 0);
                 Grid.SetRow(avatar, 0);
 
-                Grid playerName = this.FindControl<Grid>("PlayerName");
+
                 //playerName.Margin = new Avalonia.Thickness(150, 0, 0, 200);
 
                 //Grid matchesGrid = this.FindControl<Grid>("MatchesGrid");
                 //ItemsControl matchesHeaderGrid = this.FindControl<ItemsControl>("MatchesHeaderGrid");
 
             }
+        }
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+          
         }
     }
 }
